7bfd300abbb5b22571b03c1ab4329897b5f4608c
Chris Tosswill <ctosswill@engineyard.com>
2011-06-16 10:17:21 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CwEy::Application.config.secret_token = 'd4e80e396183c764a95ce3bebbd05394d981a771e0af83bb5e12f7b1c201f09934b0b9aab4d4f93466a4b0c6aa872451d34202f891ccf2c097a302c736f4495a'
